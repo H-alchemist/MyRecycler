adf32665c838e7b247cbb552dd1a55da5caae99c
echchabli_hamza <echchablihamza1@gmail.com>
2022-11-14 13:13:57 +0100

    demo

---
+++ app/src/main/java/com/example/myrecyclerviewdemo/Myadapter.java
@@ -0,0 +1,39 @@
+package com.example.myrecyclerviewdemo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Myadapter extends RecyclerView.Adapter <MyViewHolder> {
+
+    Context context;
+    List<Item> items;
+
+    public Myadapter(Context context, List<Item> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull  MyViewHolder holder, int position) {
+        holder.nameView.setText(items.get(position).getName());
+        holder.emailView.setText(items.get(position).getEmail());
+        holder.imageView.setImageResource(items.get(position).getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+}
